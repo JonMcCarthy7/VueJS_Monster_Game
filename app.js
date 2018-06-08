831394f2fb454988df5b7fb77d9afeeff07ed959
Jon McCarthy <jtmcc93@gmail.com>
2018-06-08 13:36:23 -0700

    add specialAttack method, add monsterAttacks method to refactor monster attack, only allow 3 specialAttacks per game

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    sp: 3
   },
   methods: {
     startGame: function() {
@@ -11,29 +12,51 @@ new Vue({
         (this.playerHealth = 100),
         (this.monsterHealth = 100);
     },
+
     attack: function() {
       this.monsterHealth -= this.calculateDamage(3, 10);
       if (this.checkWin()) return;
 
-      this.playerHealth -= this.calculateDamage(5, 12);
-      this.checkWin();
+      this.monsterAttacks();
     },
-    specialAttack: function() {},
+
+    specialAttack: function() {
+      let vm = this;
+      if (vm.sp > 0) {
+        this.monsterHealth -= this.calculateDamage(10, 20);
+        if (this.checkWin()) return;
+
+        this.monsterAttacks();
+        vm.sp -= 1;
+      } else {
+        alert("You are out of special Attacks!");
+      }
+    },
+
     heal: function() {},
     giveUp: function() {},
+
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
+
+    monsterAttacks: function() {
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    },
+
     checkWin: function() {
       if (this.monsterHealth <= 0) {
         confirm("You have defeated the Monster! Play again?")
           ? this.startGame()
           : (this.gameIsRunning = false);
+        this.sp = 3;
         return true;
       } else if (this.playerHealth <= 0) {
         confirm("You have been defeated... Play again?")
           ? this.startGame()
           : (this.gameIsRunning = false);
+        this.sp = 3;
         return true;
       }
       return false;
